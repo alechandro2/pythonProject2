5b5683e101a87cd00a1137f584a05f790c55f636
alechandro2 <alex@razinkov.com>
2023-02-04 16:49:40 +0300

    commit

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '5948134449:AAFegcz8DxAr9e4TYsqmnoH2a6A05aGE_aU'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUR',
+    'евро': 'EUR'
+}
+
+
+TIMEZONE = 'Rostov on Don'
+TIMEZONE_COMMON_NAME = 'Rostov on Don'
