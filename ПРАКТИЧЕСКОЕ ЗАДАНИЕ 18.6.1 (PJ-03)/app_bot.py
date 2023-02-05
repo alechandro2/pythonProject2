ea81b0decdca57b5b71e545fccdbc62ae25933a5
alechandro2 <alex@razinkov.com>
2023-02-05 14:16:04 +0300

    Merge remote-tracking branch 'origin/master'

--- ПРАКТИЧЕСКОЕ ЗАДАНИЕ 18.6.1 (PJ-03)/app_bot.py
+++ ПРАКТИЧЕСКОЕ ЗАДАНИЕ 18.6.1 (PJ-03)/app_bot.py
@@ -36,6 +36,3 @@ def convert(message: telebot.types.Message):
     else:
         text = f'Цена {amount} {quote} в {base} - {total_base}'
         bot.send_message(message.chat.id, text)
-
-
-bot.polling()
--- ПРАКТИЧЕСКОЕ ЗАДАНИЕ 18.6.1 (PJ-03)/extentions.py
+++ ПРАКТИЧЕСКОЕ ЗАДАНИЕ 18.6.1 (PJ-03)/extentions.py
@@ -6,34 +6,31 @@ class APIException(Exception):
     pass
 
 class CryptoConverter:
-    pass
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        # if quote == base:
+        #     raise APIException(f'Невозможно перевести одинаковые валюты {base}. ')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}. ')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}. ')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}. ')
+
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}. ')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]] * float(amount)
 
 
-def convert(quote: str, base: str, amount: str):
-    # if quote == base:
-    #     raise APIException(f'Невозможно перевести одинаковые валюты {base}. ')
-
-    try:
-        quote_ticker = keys[quote]
-    except KeyError:
-        raise APIException(f'Не удалось обработать валюту {quote}. ')
-
-    try:
-        base_ticker = keys[base]
-    except KeyError:
-        raise APIException(f'Не удалось обработать валюту {base}. ')
-
-    try:
-        amount = float(amount)
-    except ValueError:
-        raise APIException(f'Не удалось обработать количество {amount}. ')
-
-    if quote == base:
-        raise APIException(f'Невозможно перевести одинаковые валюты {base}. ')
-
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-    total_base = json.loads(r.content)[keys[base]] * float(amount)
-
-
-
-    return total_base
+        return total_base
