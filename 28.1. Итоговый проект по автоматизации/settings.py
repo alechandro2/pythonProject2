8132f80a1e8ca0cfb0840f1cc0a23a7a1deb1c21
Александр Разиньков <121687908+alechandro2@users.noreply.github.com>
2023-03-11 22:04:02 +0300

    Add files via upload
    commit

---
+++ 28.1. Итоговый проект по автоматизации/settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = '/TESTS/chromedriver.exe'
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'Ввести валидный email'       # Ввести валидный email
+    valid_password = 'Ввести валидный пароль'   # Ввести валидный пароль
+    valid_email_reg = 'Ввести валидный email'   # Ввести валидный email
+    invalid_email = 'alex@777.ru'
+    invalid_password = 'paSss1234568'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Мистер'
+    last_name = 'Твистер'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'г'
+    latin_generate_string = 'abcdefg'
+    chinese_chars = '形聲字象形字'
+    special_chars = '@#$%*&!?&+-]['
+    numbers = 150
+    empty = ''
+    passw1 = 'PgdM74t^'
+    passw2 = 'Zwa987456vBn09Ds'
+    passw3 = 'TrsO739BYTPYTr'
